0f03c9d5ff82fe186ff5cdd7d1c3517cc5af1577
rayen.benjilani2020@gmail.com <rayen.benjilani2020@gmail.com>
2024-10-16 11:35:47 +0100

    react-hooks

---
+++ react-hooks/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+import MovieList from "./Components/MovieList";
+import { moviesData } from "./data";
+import AddMovie from "./Components/AddMovie";
+import FilterName from "./Components/FilterName";
+import FilterRate from "./Components/Rating";
+
+function App() {
+	const [movies, setMovies] = useState(moviesData);
+	const [inputSearch, setInputSearch] = useState("");
+	const [rating, setRating] = useState(1);
+
+	const add = (newMovie) => {
+		setMovies([...movies, newMovie]);
+	};
+	return (
+		<div className='App'>
+			<h1>Movie App</h1>
+			<FilterName inputSearch={inputSearch} setInputSearch={setInputSearch} />
+			<FilterRate rating={rating} setRating={setRating} isMovieRating={false} />
+			<AddMovie add={add} />
+			<MovieList movies={movies} inputSearch={inputSearch} rating={rating} />
+		</div>
+	);
+}
+
+export default App;
---
+++ react-hooks/src/Components/MovieCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "./MovieCard.css";
+import MovieRating from "./Rating";
+
+const MovieCard = ({ movie }) => {
+	return (
+		<div className='moviecard'>
+			<Card style={{ width: "18rem" }}>
+				<Card.Img className='img-card' variant='top' src={movie.posterUrl} />
+				<Card.Body>
+					<Card.Title>{movie.title}</Card.Title>
+					<Card.Text className='desc'>{movie.description}</Card.Text>
+					<MovieRating isMovieRating={true} movieRating={movie.rate} />
+					<Button variant='primary'>Go somewhere</Button>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
+export default MovieCard;
